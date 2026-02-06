4f655e94b80344bc9c1531d42ab5969a9822a7bd
Jeff <140712389+manemajef@users.noreply.github.com>
2026-02-06 14:21:59 +0200

    mobile list retouch

--- src/ui/components/list-view.tsx
+++ src/ui/components/list-view.tsx
@@ -12,38 +12,44 @@ export function ListView(props: {
   files: FileInfo[];
 }): React.JSX.Element {
   const { app, sourcePath, files } = props;
+  const n = files.length;
 
   return (
     <div
       className={`explorer-list-container ${FANCY_LIST ? "use-fancy-list" : ""}`}
     >
-      {files.map((fileInfo) => (
-        <li
-          key={fileInfo.file.path}
-          className={`explorer-list${fileInfo.isPinned ? " pinned" : ""}`}
-          onClick={() =>
-            (Platform.isMobile || FANCY_LIST) &&
-            app.workspace.openLinkText(fileInfo.file.path, "", false)
-          }
-        >
-          <span className="list-bullet" />
-          <Group justify="between">
-            <InternalLink
-              app={app}
-              sourcePath={sourcePath}
-              path={fileInfo.file.path}
-              text={
-                fileInfo.file.extension === "md"
-                  ? fileInfo.file.basename
-                  : `${fileInfo.file.basename}.${fileInfo.file.extension}`
-              }
-            />
-            {fileInfo.file.extension !== "md" && (
-              <Badge variant="ext-filled">{fileInfo.file.extension}</Badge>
+      {files.map((fileInfo, i) => (
+        <div className="list-item-container">
+          <li
+            key={fileInfo.file.path}
+            className={`explorer-list${fileInfo.isPinned ? " pinned" : ""}`}
+            onClick={() =>
+              (Platform.isMobile || FANCY_LIST) &&
+              app.workspace.openLinkText(fileInfo.file.path, "", false)
+            }
+          >
+            <span className="list-bullet" />
+            <Group justify="between">
+              <InternalLink
+                app={app}
+                sourcePath={sourcePath}
+                path={fileInfo.file.path}
+                text={
+                  fileInfo.file.extension === "md"
+                    ? fileInfo.file.basename
+                    : `${fileInfo.file.basename}.${fileInfo.file.extension}`
+                }
+              />
+              {fileInfo.file.extension !== "md" && (
+                <Badge variant="ext-filled">{fileInfo.file.extension}</Badge>
+              )}
+              {fileInfo.isPinned && <Badge variant="pin" />}
+            </Group>
+            {i < n - 1 && (Platform.isMobile || FANCY_LIST) && (
+              <div className="list-seperator" />
             )}
-            {fileInfo.isPinned && <Badge variant="pin" />}
-          </Group>
-        </li>
+          </li>
+        </div>
       ))}
     </div>
   );
